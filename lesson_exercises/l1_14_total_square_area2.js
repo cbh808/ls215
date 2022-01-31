837bccd53e974cf91ff68844a1ab77cb42f39cfd
cbh808 <cbh808@gmail.com>
2022-01-31 21:44:59 +0100

    refactor

--- lesson_exercises/l1_14_total_square_area2.js
+++ lesson_exercises/l1_14_total_square_area2.js
@@ -1,14 +1,23 @@
 // only sum squares in totalArea
 
-function totalArea(array) {
-  let result = array.reduce((sum, rectangle) => {
-    if (rectangle[0] === rectangle[1]) {
-      sum += rectangle[0] * rectangle[1];
-    }
-    return sum;
-  }, 0);
+// function totalArea(array) {
+//   let result = array.reduce((sum, rectangle) => {
+//     if (rectangle[0] === rectangle[1]) {
+//       sum += rectangle[0] * rectangle[1];
+//     }
+//     return sum;
+//   }, 0);
 
-  return result;
+//   return result;
+// }
+
+// alt:
+function totalArea(array) {
+  let squares = array.filter(subarray => subarray[0] === subarray[1]);
+
+  let areas = squares.map(sides => sides[0] * sides[1]);
+
+  return areas.reduce((sum, area) => sum += area);
 }
 
 let rectangles = [[3, 4], [6, 6], [1, 8], [9, 9], [2, 2]];
