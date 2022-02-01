1bd94a8dc898edc998d93c698333bb1daa2b638e
cbh808 <cbh808@gmail.com>
2022-02-01 22:07:23 +0100

    ok-est

--- lesson_exercises/l1_15_processing_releases.js
+++ lesson_exercises/l1_15_processing_releases.js
@@ -1,23 +1,3 @@
-/*
-Write Function processReleaseData that processes following movie release data:
-Function to generate Array of Objects containing only id & title key/value pairs
-Assume that id when existingis an integer greater than 0
-Assume that title when existing is a string
-Rules:
-Keep only releases that have both id and title property present.
-Keep only the id and title data for each release.
-
-Data:
-input: array of Objects
-output: new array of Objects with selected k/v pairs from first object
-
-Algorithm:
-create function taking array as arg
-filter Object for Objects having both a `id` key & 'title' key
-map filtered array to return only k/v pairs id & title
-return filtered & transformed array
-*/
-
 function processReleaseData(releases) {
   return releases.filter(release => {
     if (release['id'] && release['title'])return release;
