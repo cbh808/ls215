9643ee1d2318cd4fe368822db483140d0b352449
cbh808 <cbh808@gmail.com>
2022-02-04 11:48:38 +0100

    ok

--- lesson_exercises/l1_18_bands.js
+++ lesson_exercises/l1_18_bands.js
@@ -17,6 +17,16 @@ Example:
 
 Algorithm:
 Create function with array arg, returns cleaned up array (mutate original array)
+Iterate over array, passing each object to a callback function
+Use helper to change country
+  reassign country to Canada
+User helper to capitalize
+  capitalize name
+Use Helper Method to Iterate over array
+  Iterate over each value accessed by name key of the object
+  Convert string to array of single words
+  transform any character dot at end of word by removing it
+  join back chars to string with a space between words
 
 */
 let bands = [
@@ -25,9 +35,42 @@ let bands = [
   { name: 'a silver mt. zion', country: 'Spain', active: true },
 ];
 
-function processBands(data) {
-  
-  return data;
+function changeCountry(band) {
+  band.country = 'Canada';
+  return band;
+}
+
+function capitalizeName(band) {
+  let bandName = band.name.split(' ').map(word => {
+    return word[0].toUpperCase() + word.slice(1, word.length);
+  });
+  band.name = bandName.join(' ')
+  return band
+}
+
+function removeDot(band) {
+  let words = band.name.split(' ');
+
+  words = words.map(word => {
+    if (word[word.length - 1] === '.') {
+      return word.slice(0, word.length - 1);
+    } else {
+      return word;
+    }
+  });
+
+  band.name = words.join(' ');
+  return band
+}
+
+function processBands(bands) {
+  return bands.map(band => {
+    changeCountry(band);
+    capitalizeName(band);
+    removeDot(band);
+    return band;
+  });
 }
 
 console.log(processBands(bands));
+console.log(bands);
